5e964bb12a31402b9f574ddf6bf5809a8a892da8
Maxim Gribanov <gribmax@gmail.com>
2023-08-31 00:59:14 +0300

    sh

--- config-scripts/install_mongodb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Run this script with sudo
-echo "Install MongoDB"
-sudo apt update && sudo apt install -y mongodb
-echo "Start and enable MongoDB"
-sudo systemctl start mongodb
-sudo systemctl enable mongodb
-echo "view mongoDB"
-sudo systemctl status mongodb
