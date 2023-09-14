396ad993862ef0491194e57946a9082715473826
Maxim Gribanov <gribmax@gmail.com>
2023-09-15 02:03:53 +0300

    add workhome 7

---
+++ terraform/modules/db/variables.tf
@@ -0,0 +1,30 @@
+
+variable "zone" {
+  description = "Zone"
+  # Значение по умолчанию
+  default = "ru-central1-a"
+}
+variable "public_key_path" {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+
+
+# variable "subnet_id" {
+#   description = "var.subnet_id"
+# }
+
+
+variable "app_disk_image" {
+  description = "Disk image for reddit app"
+  default     = "app_db_disk_image"
+}
+
+variable "db_disk_image" {
+  description = "var.image_id"
+  default     = "db_disk_image"
+}
+
+variable subnet_id {
+description = "Subnets for modules"
+}
