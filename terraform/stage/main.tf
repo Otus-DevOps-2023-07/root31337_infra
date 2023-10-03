548c8c643b8b7552eecbc74750ec1ec8ca27ffa8
Maxim Gribanov <gribmax@gmail.com>
2023-10-03 21:31:05 +0300

    terraform-2

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,33 +1,36 @@
-terraform {
-required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.95.0"
-    }
-  }
-}
-
-module "vpc" {
-  source = "../modules/vpc"
-}
-
+#terraform {
+ # required_providers {
+ #   yandex = {
+ #     source = "yandex-cloud/yandex"
+ #     version = "~>0.98.0"
+ #   }
+ # }
+#}
 provider "yandex" {
- service_account_key_file = var.service_account_key_file
- cloud_id = var.cloud_id
- folder_id = var.folder_id
- zone = var.zone
+service_account_key_file = var.service_account_key_file
+cloud_id = var.cloud_id
+folder_id = var.folder_id
+zone = var.zone
 }
 module "app" {
- source = "../modules/app"
- subnet_id = module.vpc.vpc_id
- public_key_path = var.public_key_path
- app_disk_image = var.app_disk_image
-
+source = "../modules/app"
+public_key_path = var.public_key_path
+app_disk_image = var.app_disk_image
+subnet_id = var.subnet_id
+service_account_key_file = var.service_account_key_file
+folder_id                = var.folder_id
+image_id                 = var.image_id
+cloud_id                 = var.cloud_id
+private_key_path         = var.private_key_path
 }
 module "db" {
- source = "../modules/db"
- subnet_id = module.vpc.vpc_id
- public_key_path = var.public_key_path
- db_disk_image = var.db_disk_image
-
+source = "../modules/db"
+public_key_path = var.public_key_path
+db_disk_image = var.db_disk_image
+subnet_id = var.subnet_id
+service_account_key_file = var.service_account_key_file
+folder_id                = var.folder_id
+image_id                 = var.image_id
+cloud_id                 = var.cloud_id
+private_key_path         = var.private_key_path
 }
