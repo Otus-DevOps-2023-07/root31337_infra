bb9a59b84966d98795132dd73b2d75d28d58cd33
Maxim Gribanov <gribmax@gmail.com>
2023-10-03 23:13:05 +0300

    terraform-2

--- terraform/modules/db/variables.tf
+++ terraform/modules/db/variables.tf
@@ -1,19 +1,35 @@
-
+variable "cloud_id" {
+  description = "Cloud"
+}
+variable "folder_id" {
+  description = "Folder"
+}
 variable "zone" {
   description = "Zone"
   # Значение по умолчанию
   default = "ru-central1-a"
 }
-variable "public_key_path" {
+variable "public_key" {
   # Описание переменной
   description = "Path to the public key used for ssh access"
 }
 
+variable "service_account_key_file" {
+  description = "service_account_key_file"
+}
 
-# variable "subnet_id" {
-#   description = "var.subnet_id"
-# }
 
+variable "private_key" {
+  description = "private_key"
+}
+
+variable "subnet_id" {
+  description = "var.subnet_id"
+}
+
+variable "image_id" {
+  description = "var.image_id"
+}
 
 variable "app_disk_image" {
   description = "Disk image for reddit app"
@@ -23,8 +39,4 @@ variable "app_disk_image" {
 variable "db_disk_image" {
   description = "var.image_id"
   default     = "db_disk_image"
-}
-
-variable subnet_id {
-description = "Subnets for modules"
 }
