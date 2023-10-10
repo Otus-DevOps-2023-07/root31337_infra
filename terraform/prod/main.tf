50c4eb6f2087043148844e6a193d087cbde552c1
Maxim Gribanov <gribmax@gmail.com>
2023-10-10 20:19:48 +0300

    test

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,9 +1,9 @@
 terraform {
-     required_providers {
-    yandex = {
-    source  = "yandex-cloud/yandex"
-    version = "0.95.0"
-  }
+ required_providers {
+   yandex = {
+     source  = "yandex-cloud/yandex"
+     version = "0.95.0"
+   }
  }
 }
 provider "yandex" {
